45e05d527385f5706cc2d018c898861520b7f423
Hernandes <hernandeshass910@gmail.com>
2025-07-14 10:49:40 -0300

    feat: criar componente de card filme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
   return (
     <>
+      <MovieCard movie={{
+        src: "a",
+        title: "Batman",
+        release_date: "2022"
+      }}/>
     </>
   )
 }
